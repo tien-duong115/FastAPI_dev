48b8dcb5ad0d7bb1d0a209b3489d870b8fc57bc5
tienduong-115 <tien.lyf.d@gmail.com>
2021-12-03 22:58:14 -0800

    Fixed alembic db intergration issues

--- alembicDir/env.py
+++ alembicDir/env.py
@@ -9,9 +9,7 @@ from app.model import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option(
-    "sqlalchemy.url", f'postgresql://{setting.database_username}:{setting.database_password}@{setting.database_port}/{setting.database_name}' 
-                        )
+config.set_main_option("sqlalchemy.url", f"postgresql://{setting.database_username}:{setting.database_password}@{setting.database_port}/{setting.database_name}")
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 fileConfig(config.config_file_name)
