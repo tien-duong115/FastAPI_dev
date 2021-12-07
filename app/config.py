aa1e3f5b6a6b95d9fdce29c1f8a33b7afd62a889
tienduong-115 <tien.lyf.d@gmail.com>
2021-12-06 21:16:58 -0800

    db config

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic import BaseSettings
 
 class Setting(BaseSettings):
     database_hostname: str
-    database_port: str
+    database_port: int
     database_password: str
     database_name: str
     database_username: str
